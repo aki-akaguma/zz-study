077edb9dc6a3df2ff1f604a438357443b2b53129
aki <aki.akaguma@hotmail.com>
2023-02-06 08:59:50 +0900

    added `test2::invoke_shell()`

--- tests/test.rs
+++ tests/test.rs
@@ -8,3 +8,30 @@ mod test1 {
         assert_eq!(sum(3i64, 5i64), 8i64);
     }
 }
+
+mod test2 {
+    use std::process::Command;
+    //
+    #[test]
+    fn invoke_shell() {
+        let output = if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+        };
+
+        let hello = output.stdout;
+        let hello = String::from_utf8_lossy(&hello).to_string();
+        #[cfg(windows)]
+        let hello = hello.replace("\r\n", "\n");
+        //
+        assert_eq!(hello, "hello\n");
+    }
+}
